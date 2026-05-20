c7dc18db23a8a4ee8a9d6f8cc2ef211f849e84f1
Jeremy Dutton <jeremydutton5@gmail.com>
2026-05-20 15:28:34 -0400

    amend astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    
   },
+
+  base: '/northjay-tailwind/', // <-- Add this exact line!
+
+
+
 });
